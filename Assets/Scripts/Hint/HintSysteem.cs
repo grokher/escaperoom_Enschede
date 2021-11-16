0c606d5762c4d5f77813933676a472715a086091
Max <Max.abbink@hotmail.com>
2021-11-16 10:20:44 +0100

    pushy

--- Assets/Scripts/Hint/HintSysteem.cs
+++ Assets/Scripts/Hint/HintSysteem.cs
@@ -15,6 +15,18 @@ public class HintSysteem : MonoBehaviour
     public GameObject MachineKamerInfo;
     public GameObject ArchiefKamerInfo;
     public GameObject TorenInfo;
+
+    public GameObject koffiekamerButton;
+    public GameObject MozaiekKamerButton;
+    public GameObject BurgerZaalButton;
+    public GameObject RaadZaalButton;
+    public GameObject TrouwZaalButton;
+    public GameObject BurgermeestersKamerButton;
+    public GameObject FriedhoffButton;
+    public GameObject MachineKamerButton;
+    public GameObject ArchiefKamerButton;
+    public GameObject TorenButton;
+
     private bool koffiekamerBool = true;
     private bool MozaiekKamerBool = true;
     private bool BurgerZaalBool = true;
@@ -26,67 +38,170 @@ public class HintSysteem : MonoBehaviour
     private bool ArchiefKamerBool = true;
     private bool TorenBool = true;
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        koffiekamerInfo.SetActive(false);
+      MozaiekKamerInfo.SetActive(false);
+        BurgerZaalInfo.SetActive(false);
+        RaadZaalInfo.SetActive(false);
+        TrouwZaalInfo.SetActive(false);
+        BurgermeestersKamerInfo.SetActive(false);
+        FriedhoffInfo.SetActive(false);
+        MachineKamerInfo.SetActive(false);
+        ArchiefKamerInfo.SetActive(false);
+        TorenInfo.SetActive(false);
     }
-
+    public void page1()
+    {
+        if (!koffiekamerBool)
+        {
+            koffiekamerInfo.SetActive(true);
+            MozaiekKamerInfo.SetActive(false);
+            BurgerZaalInfo.SetActive(false);
+            RaadZaalInfo.SetActive(false);
+            TrouwZaalInfo.SetActive(false);
+            BurgermeestersKamerInfo.SetActive(false);
+            FriedhoffInfo.SetActive(false);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+        }
+    }
+    public void page2()
+    {
+        if (!BurgerZaalBool)
+        {
+            BurgerZaalInfo.SetActive(true);
+            RaadZaalInfo.SetActive(false);
+            TrouwZaalInfo.SetActive(false);
+            BurgermeestersKamerInfo.SetActive(false);
+            FriedhoffInfo.SetActive(false);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+             koffiekamerInfo.SetActive(false);
+            MozaiekKamerInfo.SetActive(false);
+        }
+    }
+    public void page3()
+    {
+        if (!BurgermeestersKamerBool)
+        {
+            BurgermeestersKamerInfo.SetActive(true);
+            FriedhoffInfo.SetActive(false);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+            koffiekamerInfo.SetActive(false);
+            MozaiekKamerInfo.SetActive(false);
+            BurgerZaalInfo.SetActive(false);
+            RaadZaalInfo.SetActive(false);
+            TrouwZaalInfo.SetActive(false);
+        }
+    }
+    public void page4()
+    {
+        if (!FriedHoffBool)
+        {
+            FriedhoffInfo.SetActive(true);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+            koffiekamerInfo.SetActive(false);
+            MozaiekKamerInfo.SetActive(false);
+            BurgerZaalInfo.SetActive(false);
+            RaadZaalInfo.SetActive(false);
+            TrouwZaalInfo.SetActive(false);
+            BurgermeestersKamerInfo.SetActive(false);
+        }
+    }
+    public void page5()
+    {
+        if (!RaadZaalBool)
+        {
+            RaadZaalInfo.SetActive(true);
+            TrouwZaalInfo.SetActive(false);
+            BurgermeestersKamerInfo.SetActive(false);
+            FriedhoffInfo.SetActive(false);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+            koffiekamerInfo.SetActive(false);
+            MozaiekKamerInfo.SetActive(false);
+            BurgerZaalInfo.SetActive(false);
+        }
+    }
+    public void page6()
+    {
+        if (!MozaiekKamerBool)
+        {
+            MozaiekKamerInfo.SetActive(true);
+            BurgerZaalInfo.SetActive(false);
+            RaadZaalInfo.SetActive(false);
+            TrouwZaalInfo.SetActive(false);
+            BurgermeestersKamerInfo.SetActive(false);
+            FriedhoffInfo.SetActive(false);
+            MachineKamerInfo.SetActive(false);
+            ArchiefKamerInfo.SetActive(false);
+            TorenInfo.SetActive(false);
+            koffiekamerInfo.SetActive(false);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (GameObject.Find("TrouwZaal")&&TrouwZaalBool)
         {
             TrouwZaalBool = false;
-            TrouwZaalInfo.SetActive(true);
+            TrouwZaalButton.SetActive(true);
         }
         if (GameObject.Find("OudeKoffieKamer")&&koffiekamerBool)
         {
             koffiekamerBool = false;
-            koffiekamerInfo.SetActive(true);
+            koffiekamerButton.SetActive(true);
         }
         if (GameObject.Find("BurgerZaal3")&&BurgerZaalBool)
         {
             BurgerZaalBool = false;
-            BurgerZaalInfo.SetActive(true);
+            BurgerZaalButton.SetActive(true);
         }
         if (GameObject.Find("Mozaiekkamer")&&MozaiekKamerBool)
         {
             MozaiekKamerBool = false;
-            MozaiekKamerInfo.SetActive(true);
+            MozaiekKamerButton.SetActive(true);
         }
         if (GameObject.Find("Raadzaal")&&RaadZaalBool)
         {
             RaadZaalBool = false;
-            RaadZaalInfo.SetActive(true);
+            RaadZaalButton.SetActive(true);
         }
         if (GameObject.Find("BurgermeesterKamer")&&BurgermeestersKamerBool)
         {
             BurgermeestersKamerBool = false;
-            BurgermeestersKamerInfo.SetActive(true);
+            BurgermeestersKamerButton.SetActive(true);
         }
         if (GameObject.Find("FriedHoffKamer")&&FriedHoffBool)
         {
             FriedHoffBool = false;
-            FriedhoffInfo.SetActive(true);
+            FriedhoffButton.SetActive(true);
         }
         if (GameObject.Find("machinekamer")&&MachineKamerBool)
         {
             MachineKamerBool = false;
-            MachineKamerInfo.SetActive(true);
+            MachineKamerButton.SetActive(true);
         }
         if (GameObject.Find("Archief")&&ArchiefKamerBool)
         {
             ArchiefKamerBool = false;
-            ArchiefKamerInfo.SetActive(true);
+            ArchiefKamerButton.SetActive(true);
         }
    /*     if (GameObject.Find("Toren")&&TorenBool)
         {
         
         }
    */
+
+
     }
 }
