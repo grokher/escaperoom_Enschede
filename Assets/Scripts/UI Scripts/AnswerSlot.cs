2f3fe739705c883ecf3dbae635ad622bcbf564a5
saltypacifist <5laurensvaneijk@gmail.com>
2021-11-24 14:52:49 +0100

    Update AnswerSlot.cs

--- Assets/Scripts/UI Scripts/AnswerSlot.cs
+++ Assets/Scripts/UI Scripts/AnswerSlot.cs
@@ -9,6 +9,7 @@ public class AnswerSlot : MonoBehaviour, IDropHandler
     public string correctAnswer;
     public GameObject[] dragableTexts;
     public int correctslotCounter = 0;
+    public int inslotCounter = 0;
     void Start()
     {
          dragableTexts = GameObject.FindGameObjectsWithTag("DragableText");
@@ -35,20 +36,35 @@ public class AnswerSlot : MonoBehaviour, IDropHandler
         }
         foreach (var dragableText in dragableTexts)
         {
-            if (dragableText.GetComponent<DragDrop>().inCorrectSlot == false)
+            if (dragableText.GetComponent<DragDrop>().inSlot == true)
             {
-                dragableText.GetComponent<DragDrop>().inSlot = false;
-                dragableText.GetComponent<DragDrop>().CheckPos();
+                inslotCounter++;
+                if (dragableText.GetComponent<DragDrop>().inCorrectSlot == true)
+                {
+                    correctslotCounter++;
+                }
+            }
+        }
+        if(inslotCounter >= 3)
+        {
+            if (correctslotCounter >= 3)
+            {
+                dragableTexts[0].GetComponent<DragDrop>().win();
             }
             else
             {
-                correctslotCounter++;
+                foreach (var dragableText in dragableTexts)
+                {
+                    dragableText.GetComponent<DragDrop>().inSlot = false;
+                    dragableText.GetComponent<DragDrop>().CheckPos();
+                }
             }
         }
-        if (correctslotCounter >= 3)
+        else
         {
-            dragableTexts[0].GetComponent<DragDrop>().win();
+            correctslotCounter = 0;
+            inslotCounter = 0;
         }
-        correctslotCounter = 0;
+        
     }
 }
