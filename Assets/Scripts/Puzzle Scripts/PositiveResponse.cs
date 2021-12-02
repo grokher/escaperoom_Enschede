a3aabe7b3d5d745d75730043082a8d87fc05b9d6
saltypacifist <5laurensvaneijk@gmail.com>
2021-12-02 10:26:52 +0100

    quick update

---
+++ Assets/Scripts/Puzzle Scripts/PositiveResponse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PositiveResponse : MonoBehaviour
+{
+    #region
+    GameObject Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    public AudioClip rewardingSound;
+    #endregion
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Response()
+    {
+        
+    }
+}
