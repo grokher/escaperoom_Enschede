0d84e2f5ba3327166f876ddfec808467bcd5b47f
grokher <47322787+grokher@users.noreply.github.com>
2021-12-07 16:08:00 +0100

    added some more text in tutorial

--- Assets/Scripts/Tutorial/TutorialScript.cs
+++ Assets/Scripts/Tutorial/TutorialScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TutorialScript : MonoBehaviour
 {
@@ -10,7 +11,9 @@ public class TutorialScript : MonoBehaviour
     public GameObject bookPickup;
     public GameObject inspectTool;
     public TextMesh tutorialsText;
+    public TextMeshProUGUI pickupText;
     public bool Key;
+    public bool pickingupText;
 
     private void Update()
     {
@@ -24,6 +27,13 @@ public class TutorialScript : MonoBehaviour
         {
             tutorialsText.text = "je hebt de sleutel gevonden \n maak nu de deur open"; // \n = newline \t = tab \b = backspace
             Key = true;
+            if(Key == true && !pickingupText)
+            {
+                pickupText.text = "Je hebt een logboek ontvangen";
+                yield return new WaitForSeconds(3f);
+                pickupText.text = "";
+                pickingupText = true;
+            }
         }
         yield return new WaitForSeconds(2f); 
    }
