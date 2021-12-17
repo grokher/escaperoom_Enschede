b0f7ad70f7d0295176a4a33b9f12eb1b8b6bf4dc
grokher <47322787+grokher@users.noreply.github.com>
2021-12-17 15:35:36 +0100

    rode stippen toegevoegd voor de locaties van de ramen
    de stippen geven aan waar je ze kan vinden geef hier even feedback/informatie over wat misschien beter kan. en anders ook over de plattegrond van de kelder

--- Assets/Scripts/MapScript.cs
+++ Assets/Scripts/MapScript.cs
@@ -5,17 +5,26 @@ using UnityEngine.UI;
 
 public class MapScript : MonoBehaviour
 {
-    public GameObject mainFloor, firstFloor;
+    public GameObject mainFloor, firstFloor, baseMent;
 
-    public void floorButtonMain0()
+    public void floorButtonMain1()
     {
         firstFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 0.2f);
         mainFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 1);
+        baseMent.GetComponentInChildren<Image>().color = new UnityEngine.Color(1, 1, 1, 0.2f);
     }
 
-    public void floorButton1()
+    public void floorButton2()
     {
         mainFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 0.2f);
         firstFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 1);
+        baseMent.GetComponentInChildren<Image>().color = new UnityEngine.Color(1, 1, 1, 0);
+    }
+
+    public void floorButton0()
+    {
+        mainFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 0.2f);
+        firstFloor.GetComponent<Image>().color = new UnityEngine.Color(1, 1, 1, 0);
+        baseMent.GetComponentInChildren<Image>().color = new UnityEngine.Color(1, 1, 1, 1);
     }
 }
