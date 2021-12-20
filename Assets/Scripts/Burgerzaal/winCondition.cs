31e16386bc3c2e96291e97e7de1adf63cca25616
Max <Max.abbink@hotmail.com>
2021-12-20 12:09:59 +0100

    txt fix zdv

--- Assets/Scripts/Burgerzaal/winCondition.cs
+++ Assets/Scripts/Burgerzaal/winCondition.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class winCondition : MonoBehaviour
 {
     public int winCounter = 0;
+    public TextMesh winText;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,6 @@ public class winCondition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        winText.text = winCounter.ToString();
     }
 }
--- Assets/Scripts/win/raamScript.cs
+++ Assets/Scripts/win/raamScript.cs
@@ -7,6 +7,7 @@ public class raamScript : MonoBehaviour
     public MouseSelect mouse;
     public winCondition win;
 
+
     // Start is called before the first frame update
     void Start()
     {
