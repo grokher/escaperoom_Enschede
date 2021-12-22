a4b97a371c3b63125acd9abea93f8e51dbb976e0
grokher <47322787+grokher@users.noreply.github.com>
2021-12-22 14:43:00 +0100

    ramen kan je oppakken + text voor mozaiek deur

--- Assets/Scripts/skybox changes/skyboxRotation.cs
+++ Assets/Scripts/skybox changes/skyboxRotation.cs
@@ -7,10 +7,13 @@ public class skyboxRotation : MonoBehaviour
 {
     //raycast hit -> moves next specific area
     public Material skyboxArea;
+    public GameObject mozaiekArrow;
     public GameObject CurrentArea;
     public GameObject NextArea;
     public TextMeshProUGUI locationTexts;
+    public TextMeshProUGUI lockedPickupText;
     public GameObject bookText;
+    private bool stopText;
     public void NextAreaOnClick()
     {
         Debug.Log("Change F Area");
@@ -19,10 +22,31 @@ public class skyboxRotation : MonoBehaviour
         NextArea.SetActive(true);
         locationTexts.text = NextArea.name;
         bookText.SetActive(false);
+        
+        
+    }
+
+    IEnumerator lockedDoor()
+    {
+        Debug.Log("HITTY");
+        if (Camera.main.GetComponent<MouseSelect>().hitObject.name == mozaiekArrow.name && !stopText)
+        {
+            stopText = false;
+            Debug.Log("Text HIt");
+            lockedPickupText.text = "je hebt een sleutel nodig om de deur te openen";
+            yield return new WaitForSeconds(3f);
+            lockedPickupText.text = "";
+            stopText = true;
+            
+        }
+        if (Camera.main.GetComponent<MouseSelect>().raycastHit.name != mozaiekArrow.name)
+        {
+            stopText = false;
+        }
     }
 
     public void Update()
     {
-        
+        StartCoroutine(lockedDoor());
     }
 }
--- Assets/Scripts/win/raamScript.cs
+++ Assets/Scripts/win/raamScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class raamScript : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class raamScript : MonoBehaviour
     public GameObject raam, raam1, raam2, raam3, raam4, raam5, raam6;
     private int raamCount = 0;
     private bool hasRaam, hasRaam1, hasRaam2, hasRaam3, hasRaam4, hasRaam5, hasRaam6;
+    public TextMeshProUGUI pickUpText;
 
 
     // Start is called before the first frame update
@@ -20,48 +22,71 @@ public class raamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(windowPickup());
+    }
+    IEnumerator windowPickup()
+    {
         if (raam.GetComponent<pickupableItems>().hasThisItem && !hasRaam)
         {
             raamCount++;
             hasRaam = true;
-            Debug.Log("Ramen amount " + raamCount);
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam1.GetComponent<pickupableItems>().hasThisItem && !hasRaam1)
         {
             raamCount++;
             hasRaam1 = true;
-            Debug.Log("Ramen amount " + raamCount);
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam2.GetComponent<pickupableItems>().hasThisItem && !hasRaam2)
         {
             raamCount++;
             hasRaam2 = true;
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam3.GetComponent<pickupableItems>().hasThisItem && !hasRaam3)
         {
             raamCount++;
             hasRaam3 = true;
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam4.GetComponent<pickupableItems>().hasThisItem && !hasRaam4)
         {
             raamCount++;
             hasRaam4 = true;
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam5.GetComponent<pickupableItems>().hasThisItem && !hasRaam5)
         {
             raamCount++;
             hasRaam5 = true;
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
         else if (raam6.GetComponent<pickupableItems>().hasThisItem && !hasRaam6)
         {
             raamCount++;
             hasRaam6 = true;
+            pickUpText.text = "Je hebt een raam gevonden";
+            yield return new WaitForSeconds(3f);
+            pickUpText.text = "";
         }
-        
+
         if (raamCount >= 7)
         {
             //win screen
+            //fade naar burgerzaal
         }
     }
-
 }
