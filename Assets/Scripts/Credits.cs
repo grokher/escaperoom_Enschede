0ed77fb973b78b2c85e7f42a065580591ef709b6
grokher <47322787+grokher@users.noreply.github.com>
2022-01-06 13:41:57 +0100

    1st batch bug fixes

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -11,6 +11,7 @@ public class Credits : MonoBehaviour
         if(Camera.main.GetComponent<MouseSelect>().activated && Camera.main.GetComponent<MouseSelect>().raycastHit.name == gameObject.name)
         {
             credits.SetActive(true);
+            Camera.main.GetComponent<MouseSelect>().activated = false;
         }
     }
 }
