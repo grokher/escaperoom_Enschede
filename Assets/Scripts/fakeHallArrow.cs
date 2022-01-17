3599d99fcea854b3efcb0e2d6211e38db051e9cb
grokher <47322787+grokher@users.noreply.github.com>
2022-01-17 16:28:10 +0100

    continues text

--- Assets/Scripts/fakeHallArrow.cs
+++ Assets/Scripts/fakeHallArrow.cs
@@ -6,16 +6,28 @@ public class fakeHallArrow : MonoBehaviour
 {
     public Camera mainCamera;
     public GameObject text;
+    public TextMesh textChange;
+    int count = 0;
     // Start is called before the first frame update
     public void Update()
     {
-        if (mainCamera.GetComponent<MouseSelect>().activated && mainCamera.GetComponent<MouseSelect>().raycastHit.name == gameObject.name)
+        if (count >= 1 && mainCamera.GetComponent<MouseSelect>().activated && mainCamera.GetComponent<MouseSelect>().raycastHit.name == gameObject.name)
         {
-            text.SetActive(true);
+            mainCamera.GetComponent<MouseSelect>().activated = false;
+            textChange.text = "Bureaucratius blokkeert deze hal. Wat zou de \nburgermeester doen bij het beeldje naast de trap";
+            
+            
             //gameObject.SetActive(false);
+           
+        }
+        else if (count <= 1 && mainCamera.GetComponent<MouseSelect>().activated && mainCamera.GetComponent<MouseSelect>().raycastHit.name == gameObject.name)
+        {
+            mainCamera.GetComponent<MouseSelect>().activated = false;
+            text.SetActive(true);
+            count += 1;
         }
         //Debug.Log(mainCamera.transform.eulerAngles);
-        
+
     }
     void OnDisable()
     {
