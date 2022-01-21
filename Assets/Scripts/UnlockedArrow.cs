81764bda4014a92906f2abacd06cdb5f71d077dc
skem500 <max.abbink@hotmail.com>
2022-01-21 10:06:00 +0100

    pushy

--- Assets/Scripts/UnlockedArrow.cs
+++ Assets/Scripts/UnlockedArrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UnlockedArrow : MonoBehaviour
 {
     [SerializeField] private MouseSelect select;
+    public GameObject burgerZaalSlot;
     public GameObject unlockingArrow;
     public GameObject unlockedArrow;
     public GameObject unlockedTorenArrow;
@@ -19,6 +20,7 @@ public class UnlockedArrow : MonoBehaviour
         {
             Debug.Log(select.hitObject + "hitted");
             unlockedArrow.SetActive(true);
+            burgerZaalSlot.SetActive(false);
             saveScript.Save(gameObject);
             swapObject.SetActive(false);
             //text saying you unlocked a new acces way to walk around
