a6a96108ee28904ecc2d8e731d080e8af5cf8dd1
grokher <47322787+grokher@users.noreply.github.com>
2022-01-24 16:50:06 +0100

    last push fix

--- Assets/Scripts/skybox changes/skyboxRotationLock.cs
+++ Assets/Scripts/skybox changes/skyboxRotationLock.cs
@@ -8,6 +8,7 @@ public class skyboxRotationLock : MonoBehaviour
     public GameObject mozaiekArrow;
     public GameObject torenArrow;
     public GameObject torenKey;
+    public GameObject Raam;
     public TextMeshProUGUI popUpText;
     bool archiveKey = false;
 
@@ -29,7 +30,7 @@ public class skyboxRotationLock : MonoBehaviour
     {
         if (!torenKey.GetComponent<pickupableItems>().hasThisItem && Camera.main.GetComponent<MouseSelect>().hitObject.name == torenArrow.name)
         {
-            popUpText.text = "je moet eerst de sleutel vinden voordat je hier uit kan gaan";
+            popUpText.text = "je moet eerst de sleutel en het raam vinden voordat je hier uit kan gaan";
             yield return new WaitForSeconds(2.5f);
             popUpText.text = "";
         }
